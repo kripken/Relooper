a25033e4a40cc0576a8acca26d56e8560e64b019
Alon Zakai <alonzakai@gmail.com>
2012-03-14 16:57:26 -0700

    add bindings generation for emscripten

---
+++ emscripten/test.js
@@ -0,0 +1,22 @@
+// js -m -n -e "load('relooper.js')" test.js
+
+print("-- If shape --\n");
+
+var b_a = new Block("// block A\n", "check");
+var b_b = new Block("// block B\n", "check");
+var b_c = new Block("// block C\n", "check");
+
+b_a.AddBranchTo(b_b, 10);
+b_a.AddBranchTo(b_c, 20);
+
+b_b.AddBranchTo(b_c, 30);
+
+var r = new Relooper();
+
+r.AddBlock(b_a);
+r.AddBlock(b_b);
+r.AddBlock(b_c);
+
+r.Calculate(b_a);
+r.Render();
+
