a26ba5dd90fc6dda6e01818e40053763c8e706c9
Alon Zakai <alonzakai@gmail.com>
2012-05-24 21:18:25 +0200

    fuzzer works

--- fuzzer.py
+++ fuzzer.py
@@ -1,17 +1,45 @@
 
-import random, subprocess
+import random, subprocess, difflib
 
 while True:
   # Random decisions
-  num = random.randint(1, 6)
+  num = random.randint(2, 10)
   density = random.random()
-  decisions = [random.randint(0, 1) for x in range(1000)]
+  decisions = [random.randint(1, num-1) for x in range(random.randint(round(num/3), num))]
+  branches = [0]*num
+  defaults = [0]*num
+  for i in range(num):
+    b = set([])
+    bs = random.randint(1, max(1, round(density*(num-1))))
+    for j in range(bs):
+      b.add(random.randint(1, num-1))
+    defaults[i] = random.choice(list(b))
+    b.remove(defaults[i])
+    branches[i] = b
   print num, density
 
-  # parts
-  entry = '''print('entry'); var label; var decisions = %s; var index = 0; function check() { if (index == decisions.length) throw 'HALT'; return decisions[index++] }''' % str(decisions)
+  for temp in ['fuzz', 'fuzz.fast.js', 'fuzz.cpp']:
+    try:
+      os.unlink(temp)
+    except:
+      pass
 
-  opt = '''
+  # parts
+  entry = '''print('entry'); var label; var state; var decisions = %s; var index = 0; function check() { if (index == decisions.length) throw 'HALT'; return decisions[index++] }''' % str(decisions)
+
+  slow = entry + '\n'
+  for i in range(len(branches[0])):
+    if i > 0: slow += 'else '
+    b = list(branches[0])
+    slow += 'if (state == %d) { label = %d; }\n' % (b[i], b[i]) # TODO: split range 1-n into these options
+  if len(branches[0]): slow += 'else '
+  slow += 'label = %d;\n' % defaults[0]
+
+  slow += '''
+while(1) switch(label) {
+'''
+
+  fast = '''
 
 #include <stdlib.h>
 #include "Relooper.h"
@@ -23,30 +51,37 @@ int main() {
   Relooper::SetOutputBuffer(buffer);
 '''
 
+  for i in range(1, num):
+    slow += '  case %d: print(%d); state = check(); \n' % (i, i)
+    for b in branches[i]:
+      slow += '    if (state == %d) { label = %d; break }\n' % (b, b) # TODO: split range 1-n into these fastions
+    slow += '    label = %d; break\n' % defaults[i]
+
   for i in range(num):
     if i == 0:
-      opt += '''
+      fast += '''
   Block *b%d = new Block("%s");
 ''' % (i, entry)
     else:
-      opt += '''  Block *b%d = new Block("check(); print(%d);");
+      fast += '''  Block *b%d = new Block("print(%d); state = check();");
 ''' % (i, i)
 
   for i in range(num):
-    for j in range(1, num):
-      if random.random() <= density:
-        opt += '''  b%d->AddBranchTo(b%d, "check()");
-''' % (i, j)
+    for b in branches[i]:
+      fast += '''  b%d->AddBranchTo(b%d, "state == %d");
+''' % (i, b, b)
+    fast += '''  b%d->AddBranchTo(b%d, NULL);
+''' % (i, defaults[i])
 
-  opt += '''
+  fast += '''
   Relooper r;
 '''
 
   for i in range(num):
-    opt += '''  r.AddBlock(b%d);
+    fast += '''  r.AddBlock(b%d);
 ''' % i
 
-  opt += '''
+  fast += '''
   r.Calculate(b0);
   printf("\\n\\n");
   r.Render();
@@ -57,11 +92,24 @@ int main() {
 }
 '''
 
-  open('fuzz.cpp', 'w').write(opt)
+  slow += '}'
 
+  open('fuzz.slow.js', 'w').write(slow)
+  open('fuzz.cpp', 'w').write(fast)
+  print '_',
+  slow_out = subprocess.Popen(['mozjs', '-m', '-n', 'fuzz.slow.js'], stdout=subprocess.PIPE).communicate()[0]
+
+  print '.',
   subprocess.call(['g++', 'fuzz.cpp', 'Relooper.o', '-o', 'fuzz', '-g'])
-  subprocess.call(['./fuzz'], stdout=open('fuzz.opt.js', 'w'))
-  subprocess.call(['mozjs', '-m', '-n', 'fuzz.opt.js'], stdout=open('fuzz.opt.txt', 'w'))
+  print '*',
+  subprocess.call(['./fuzz'], stdout=open('fuzz.fast.js', 'w'))
+  print '-',
+  fast_out = subprocess.Popen(['mozjs', '-m', '-n', 'fuzz.fast.js'], stdout=subprocess.PIPE).communicate()[0]
+  print
 
-  break
+  if slow_out != fast_out:
+    print ''.join([a.rstrip()+'\n' for a in difflib.unified_diff(slow_out.split('\n'), fast_out.split('\n'), fromfile='slow', tofile='fast')])
+    assert False
+
+  #break
 
