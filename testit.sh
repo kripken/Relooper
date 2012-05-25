88fbfbfcf13b0067bf2a9095294f4d60c2108287
Alon Zakai <alonzakai@gmail.com>
2012-05-25 10:46:34 +0200

    fix testit order

--- testit.sh
+++ testit.sh
@@ -1,32 +1,32 @@
 echo "test"
 ./test &> test.out
-diff -U 5 test.out test.txt
+diff -U 5 test.txt test.out
 
 echo "test 2"
 ./test2 &> test2.out
-diff -U 5 test2.out test2.txt
+diff -U 5 test2.txt test2.out
 
 echo "test 3"
 ./test3 &> test3.out
-diff -U 5 test3.out test3.txt
+diff -U 5 test3.txt test3.out
 
 echo "test debug"
 ./test_debug &> test_debug.out
-diff -U 5 test_debug.out test_debug.txt
+diff -U 5 test_debug.txt test_debug.out
 
 echo "test dead"
 ./test_dead &> test_dead.out
-diff -U 5 test_dead.out test_dead.txt
+diff -U 5 test_dead.txt test_dead.out
 
 echo "test 4"
 ./test4 &> test4.out
-diff -U 5 test4.out test4.txt
+diff -U 5 test4.txt test4.out
 
 echo "test inf"
 ./test_inf &> test_inf.out
-diff -U 5 test_inf.out test_inf.txt
+diff -U 5 test_inf.txt test_inf.out
 
 echo "test fuzz1"
 ./test_fuzz1 &> test_fuzz1.out
-diff -U 5 test_fuzz1.out test_fuzz1.txt
+diff -U 5 test_fuzz1.txt test_fuzz1.out
 
